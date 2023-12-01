59035af2d2f8748aada5dc5383d5283d8535c07c
Jake Hiltscher <jbhiltscher@gmail.com>
2023-12-01 12:22:33 -0700

    Cleaned games.csv, added process to cleaning.py

--- SteamInsights/cleaning.py
+++ SteamInsights/cleaning.py
@@ -42,7 +42,27 @@ sales['Year_of_Release'] = sales['Year_of_Release'].fillna(0).astype(int)
 
 
 # Games Dataset ------------------
+# Read in games.csv
+games_df = pd.read_csv('data/games.csv')
 
+# Remove not needed columns
+games = games_df.loc[:, ~games_df.columns.str.contains('url', case=False)]
+
+# Games that support English
+games = games[games['Supported languages'].str.contains('english', case=False)]
+
+# Remove columns
+columns_to_remove = ['Supported languages','Required age','Positive','Negative','Score rank','Achievements','Website','Support email','Reviews','Header image','Full audio languages', 'Tags', 'Screenshots', 'Movies', 'About the game', 'Peak CCU', 'Notes']
+games = games.drop(columns=columns_to_remove, errors='ignore')
+
+# Make Estimated Owners average
+# Extract minimum and maximum values using regular expressions
+owners_range = games['Estimated owners'].str.extract(r'(\d+) - (\d+)')
+min_owners = owners_range[0].astype(float)
+max_owners = owners_range[1].astype(float)
+
+# Calculate the average and replace the 'Estimated owners' column
+games['Estimated owners'] = (min_owners + max_owners) / 2
 
 
 # Ratings Dataset ------------------
