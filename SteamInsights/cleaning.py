a9d89056115b06bbdd9cfe607be336f022c2c2de
Caroline Jarman <carolinerosejarman@gmail.com>
2023-12-03 22:09:44 -0700

    fixes merge conflict with cleaning.py

--- SteamInsights/cleaning.py
+++ SteamInsights/cleaning.py
@@ -4,26 +4,21 @@ import numpy as np
 # Reviews Dataset ------------------
 
 rw = pd.read_csv('data/reviews.csv')
-reviews = rw.loc[:, ['name', 'recent_reviews', 'all_reviews',
-                     'popular_tags', 'achievements']]
+reviews = rw.loc[:, ['name', 'recent_reviews', 'all_reviews', 'popular_tags',]]
 reviews.dropna(subset=['all_reviews'], inplace=True)
-
-# use regex to separate out the numbers in recent and all reviews
 pattern = r'([a-zA-Z\s]+),\(([\d,]+)\),-\s(\d+)%' #will handle if the number has a comma
-
-# create the 3 new columns for recent and all reviews
 reviews[['recent_sentiment', 'recent_review_number', 'recent_positive_percentage']] = reviews['recent_reviews'].str.extract(pattern)
 reviews[['all_sentiment', 'all_review_number', 'all_positive_percentage']] = reviews['all_reviews'].str.extract(pattern)
-
 reviews['recent_review_number'] = reviews['recent_review_number'].str.replace(',', '')
 reviews['recent_review_number'] = pd.to_numeric(reviews['recent_review_number'])
 reviews['recent_positive_percentage'] = pd.to_numeric(reviews['recent_positive_percentage'])
-
 reviews['all_review_number'] = reviews['all_review_number'].str.replace(',', '')
 reviews['all_review_number'] = pd.to_numeric(reviews['all_review_number'])
 reviews['all_positive_percentage'] = pd.to_numeric(reviews['all_positive_percentage'])
 reviews.drop(columns=['recent_reviews', 'all_reviews'], inplace=True)
 reviews.reset_index(inplace=True)
+reviews.columns = reviews.columns.str.lower().str.replace(' ', '_')
+reviews.to_csv('data/reviews_cleaned.csv', index=False)
 
 # Sales Dataset ------------------
 
@@ -31,14 +26,15 @@ sales2016 = pd.read_csv('data/sales2016.csv')
 sales2017 = pd.read_csv('data/sales2017.csv')
 s16 = sales2016.loc[:, ['Name', 'Platform', 'Year_of_Release', 'Genre', 'Global_Sales']]
 s17 = sales2017.loc[:, ['Name', 'Platform', 'Year_of_Release', 'Genre', 'Global_Sales']]
-
 sales = pd.merge(s16, s17, on=['Name', 'Platform', 'Year_of_Release', 'Genre',], how='outer', suffixes=('_2016', '_2017'))
 sales['Global_Sales_2016'].fillna(0, inplace=True)
 sales['Global_Sales_2017'].fillna(0, inplace=True)
 sales['Global_Sales'] = sales[['Global_Sales_2016', 'Global_Sales_2017']].max(axis=1)
 sales.drop(['Global_Sales_2016', 'Global_Sales_2017'], axis=1, inplace=True)
 sales['Global_Sales'].replace(0, np.nan, inplace=True)
-sales['Year_of_Release'] = sales['Year_of_Release'].fillna(0).astype(int)
+sales = sales.groupby('Name', as_index=False)['Global_Sales'].sum()
+sales.columns = sales.columns.str.lower().str.replace(' ', '_')
+sales.to_csv('data/sales_cleaned.csv', index=False)
 
 
 # Games Dataset ------------------
@@ -52,7 +48,8 @@ games = games_df.loc[:, ~games_df.columns.str.contains('url', case=False)]
 games = games[games['Supported languages'].str.contains('english', case=False)]
 
 # Remove columns
-columns_to_remove = ['Supported languages','Required age','Positive','Negative','Score rank','Achievements','Website','Support email','Reviews','Header image','Full audio languages', 'Tags', 'Screenshots', 'Movies', 'About the game', 'Peak CCU', 'Notes']
+# columns_to_remove = ['Supported languages','Required age','Positive','Negative','Score rank','Achievements','Website','Support email','Reviews','Header image','Full audio languages', 'Tags', 'Screenshots', 'Movies', 'About the game', 'Peak CCU', 'Notes']
+columns_to_remove = ['Supported languages','Required age','Positive','Negative','Score rank','Website','Support email','Reviews','Header image','Full audio languages', 'Screenshots', 'Movies', 'About the game', 'Peak CCU', 'Notes']
 games = games.drop(columns=columns_to_remove, errors='ignore')
 
 # Make Estimated Owners average
@@ -65,5 +62,46 @@ max_owners = owners_range[1].astype(float)
 games['Estimated owners'] = (min_owners + max_owners) / 2
 
 
+
+# desired_columns = ['Name', 'Release date', 'Estimated owners', 'Price', 'Achievements', 'Windows', 'Mac', 'Linux', 'Developers', 'Publishers', 'Categories', 'Genres', 'Tags']
+# games = pd.read_csv('data/games.csv', usecols=desired_columns)
+games.dropna(subset=['Name'], inplace=True)
+games['Single Player'] = games['Categories'].str.contains('Single-player', case=False, na=False)
+games['Release date'] = pd.to_datetime(games['Release date'], format='mixed')
+games['Release Year'] = games['Release date'].dt.year
+games.drop('Release date', axis=1, inplace=True)
+games['players'] = 'NA'
+games.loc[games['Categories'].str.contains('Multi') & ~games['Categories'].str.contains('Single').isna(), 'players'] = 'multi'
+games.loc[games['Categories'].str.contains('Single') & ~games['Categories'].str.contains('Multi').isna(), 'players'] = 'single'
+games.loc[games['Categories'].str.contains('Single') & games['Categories'].str.contains('Multi'), 'players'] = 'single/multi'
+games.drop('Categories', axis=1, inplace=True)
+games.columns = games.columns.str.lower().str.replace(' ', '_')
+games.to_csv('data/games_cleaned.csv', index=False)
+
+
+
 # Ratings Dataset ------------------
 # does not need any cleaning
+
+# Joining Games, Reviews, and Sales datasets ------------------
+all_games = pd.merge(games, reviews, on='name').merge(sales, on='name')
+all_games.drop(columns = ['index'], inplace=True)
+all_games.to_csv('data/all_games_cleaned.csv', index=False)
+
+
+# Tags Dataset ------------------
+tags_split = all_games['tags'].str.split(',', expand=True)
+unique_tags = tags_split.stack().unique().tolist()
+tags_encoded = pd.DataFrame(0, index=all_games.index, columns=unique_tags)
+for tag in unique_tags:
+    tags_encoded[tag] = tags_split.apply(lambda row: 1 if tag in row.values else 0, axis=1)
+tags = pd.concat([all_games['name'], tags_encoded], axis=1)
+names = tags['name']
+t = tags.copy()
+t.set_index('name', inplace=True)
+t_sort = t.sum().sort_values(ascending=False)
+t = tags[t_sort.index]
+t['name'] = names
+t.insert(0, 'name', t.pop('name'))
+tags = t.copy()
+tags.to_csv('data/tags.csv', index=False)
---
+++ SteamInsights/summary.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+
+all_games = pd.read_csv('data/all_games_cleaned.csv')
+tags = pd.read_csv('data/tags.csv')
+
+def game_summary(game_name):
+    """
+    Retrieve a summary of the specified game.
+
+    Parameters:
+    - game_name (str): The name of the game for which to retrieve the summary.
+
+    Returns:
+    - pandas.DataFrame: A DataFrame containing the summary information for the specified game.
+      Columns included: 'name', 'publishers', 'developers', 'all_sentiment', 'global_sales', 'release_year'.
+
+    Example:
+    >>> summary = game_summary('Example Game')
+    >>> print(summary)
+               name       publishers     developers  all_sentiment  global_sales  release_year
+    123  Example Game  Example Publisher  Example Dev   Positive       10.5          2020
+    """
+    summary = all_games[all_games['name'] == game_name]
+    return summary.loc[:, ['name', 'publishers', 'developers', 'all_sentiment', 'global_sales', 'release_year']]
+# game_summary('Farming Simulator 17')
+
+
+def get_sentiment(game_name):
+    """
+    Retrieve sentiment information for the specified game.
+
+    Parameters:
+    - game_name (str): The name of the game for which to retrieve sentiment information.
+
+    Returns:
+    - pandas.DataFrame: A DataFrame containing sentiment details for the specified game.
+      Columns included: 'name', 'recent_sentiment', 'recent_review_number', 'recent_positive_percentage',
+      'all_sentiment', 'all_review_number', 'all_positive_percentage'.
+
+    Example:
+    >>> sentiment_data = get_sentiment('Example Game')
+    >>> print(sentiment_data)
+               name recent_sentiment  recent_review_number  recent_positive_percentage  all_sentiment  all_review_number  all_positive_percentage
+    123  Example Game  Mostly Positive  500                   80.0                         Positive       1000               75.0
+    """
+    sentiment = all_games[all_games['name'] == game_name]
+    return sentiment.loc[:, ['name', 'recent_sentiment', 'recent_review_number', 'recent_positive_percentage', 'all_sentiment', 'all_review_number', 'all_positive_percentage' ]]
+
+
+def get_sales_info(game_name):
+    """
+        Retrieve sales information for the specified game.
+
+        Parameters:
+        - game_name (str): The name of the game for which to retrieve sales information.
+
+        Returns:
+        - pandas.DataFrame: A DataFrame containing sales details for the specified game.
+          Columns included: 'name', 'price', 'estimated_owners', 'global_sales', 'release_year'.
+
+        Example:
+        >>> sales_data = get_sales_info('Example Game')
+        >>> print(sales_data)
+                   name  price  estimated_owners  global_sales  release_year
+        123  Example Game  29.99  5000000           10.5          2020
+        """
+    sale = all_games[all_games['name'] == game_name]
+    return sale.loc[:, ['name', 'price', 'estimated_owners', 'global_sales', 'release_year']]
+
+
+def get_genre(game_name):
+    """
+    Retrieve genre information for the specified game.
+
+    Parameters:
+    - game_name (str): The name of the game for which to retrieve genre information.
+
+    Returns:
+    - pandas.DataFrame: A DataFrame containing genre details for the specified game.
+      Columns included: 'name', 'achievements', 'single_player', 'categories', 'genres', 'tags', 'popular_tags'.
+
+    Example:
+    >>> genre_data = get_genre('Example Game')
+    >>> print(genre_data)
+               name  achievements  single_player  categories         genres                tags            popular_tags
+    123  Example Game  50            True           Action, RPG  Action, RPG, Adventure  Action, Adventure   Open World, Story Rich
+    """
+    genres = all_games[all_games['name'] == game_name]
+    return genres.loc[:, ['name', 'achievements', 'single_player', 'categories', 'genres', 'tags', 'popular_tags']]
+
+
+def get_tags(game_name):
+    """
+    Retrieve tags associated with the specified game.
+
+    Parameters:
+    - game_name (str): The name of the game for which to retrieve tags.
+
+    Returns:
+    - List[str]: A list of tags associated with the specified game.
+
+    Example:
+    >>> game_tags = get_tags('Example Game')
+    >>> print(game_tags)
+    ['Action', 'Adventure', 'Classic', 'Platformer']
+    """
+    game_tags = tags[tags['name'] == game_name]
+    return game_tags.columns[game_tags.iloc[0] == 1].tolist()
+
+
+
+
+
+
