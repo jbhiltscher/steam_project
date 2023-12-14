a21eed7d790775dad9a9e2643f32577b4d080c01
Jake Hiltscher <jbhiltscher@gmail.com>
2023-12-13 23:45:17 -0700

    fixed docstring format

--- SteamInsights/summary.py
+++ SteamInsights/summary.py
@@ -26,14 +26,16 @@ def game_summary(game_name):
     -------
     pandas.DataFrame
         A DataFrame containing the summary information for the specified game.
-        Columns included: 'name', 'publishers', 'developers', 'all_sentiment', 'global_sales', 'release_year.
+        Columns included: 'name', 'publishers', 'developers', 'all_sentiment', 'global_sales', 'release_year'.
 
-    Example:
+    Example
+    -------
     >>> summary = game_summary('Farming Simulator 17')
     >>> print(summary)
               name       publishers     developers  all_sentiment  global_sales  release_year
-    123  Example Game  Example Publisher  Example Dev   Positive       10.5          2020
+    123  Example Game  Example Publisher  Example Dev       Positive           10.5          2020
     """
+
 
     summary = all_games[all_games['name'] == game_name]
     summary.reset_index(inplace=True)
@@ -54,15 +56,16 @@ def get_sentiment(game_name):
     pandas.DataFrame
         A DataFrame containing sentiment details for the specified game.
         Columns included: 'name', 'recent_sentiment', 'recent_review_number', 'recent_positive_percentage',
-        'all_sentiment', 'all_review_number', 'all_positive_percentage.
+        'all_sentiment', 'all_review_number', 'all_positive_percentage'.
 
-    Example:
+    Example
+    -------
     >>> sentiment_data = get_sentiment('Farming Simulator 17')
     >>> print(sentiment_data)
-              name recent_sentiment  recent_review_number  recent_positive_percentage  all_sentiment  all_review_number  all_positive_percentage
-    123  Example Game  Mostly Positive  500                   80.0                         Positive       1000               75.0
-    
+              name  recent_sentiment  recent_review_number  recent_positive_percentage  all_sentiment  all_review_number  all_positive_percentage
+    123  Example Game  Mostly Positive                   500                        80.0       Positive               1000                     75.0
     """
+
 
     sentiment = all_games[all_games['name'] == game_name]
     return sentiment.loc[:, ['name', 'recent_sentiment', 'recent_review_number', 'recent_positive_percentage', 'all_sentiment', 'all_review_number', 'all_positive_percentage' ]]
@@ -72,19 +75,25 @@ def get_sales_info(game_name):
     """
     Retrieve sales information for the specified game.
 
-    Parameters:
-    - game_name (str): The name of the game for which to retrieve sales information.
+    Parameters
+    ----------
+    game_name : str
+        The name of the game for which to retrieve sales information.
 
-    Returns:
-    - pandas.DataFrame: A DataFrame containing sales details for the specified game.
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing sales details for the specified game.
         Columns included: 'name', 'price', 'estimated_owners', 'global_sales', 'release_year'.
 
-        Example:
-        >>> sales_data = get_sales_info('Farming Simulator 17')
-        >>> print(sales_data)
-                   name  price  estimated_owners  global_sales  release_year
-        123  Example Game  29.99  5000000           10.5          2020
-        """
+    Example
+    -------
+    >>> sales_data = get_sales_info('Farming Simulator 17')
+    >>> print(sales_data)
+              name  price  estimated_owners  global_sales  release_year
+    123  Example Game  29.99           5000000          10.5          2020
+    """
+
 
     sale = all_games[all_games['name'] == game_name]
     sale.reset_index(inplace=True)
@@ -95,19 +104,25 @@ def get_genre(game_name):
     """
     Retrieve genre information for the specified game.
 
-    Parameters:
-    - game_name (str): The name of the game for which to retrieve genre information.
+    Parameters
+    ----------
+    game_name : str
+        The name of the game for which to retrieve genre information.
 
-    Returns:
-    - pandas.DataFrame: A DataFrame containing genre details for the specified game.
-      Columns included: 'name', 'achievements', 'single_player', 'categories', 'genres', 'tags', 'popular_tags'.
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing genre details for the specified game.
+        Columns included: 'name', 'achievements', 'single_player', 'categories', 'genres', 'tags', 'popular_tags'.
 
-    Example:
+    Example
+    -------
     >>> genre_data = get_genre('Farming Simulator 17')
     >>> print(genre_data)
                name  achievements  single_player  categories         genres                tags            popular_tags
-    123  Example Game  50            True           Action, RPG  Action, RPG, Adventure  Action, Adventure   Open World, Story Rich
+    123  Example Game            50           True   Action, RPG  Action, RPG, Adventure  Action, Adventure   Open World, Story Rich
     """
+
 
     genres = all_games[all_games['name'] == game_name]
     genres.reset_index(inplace=True)
@@ -118,18 +133,23 @@ def get_tags(game_name):
     """
     Retrieve tags associated with the specified game.
 
-    Parameters:
-    - game_name (str): The name of the game for which to retrieve tags.
+    Parameters
+    ----------
+    game_name : str
+        The name of the game for which to retrieve tags.
 
-    Returns:
-    - List[str]: A list of tags associated with the specified game.
+    Returns
+    -------
+    List[str]
+        A list of tags associated with the specified game.
 
-    Example:
+    Example
+    -------
     >>> game_tags = get_tags('Farming Simulator 17')
     >>> print(game_tags)
     ['Action', 'Adventure', 'Classic', 'Platformer']
-
     """
+
 
     game_tags = tags[tags['name'] == game_name]
     game_tags.reset_index(inplace=True)
@@ -140,19 +160,25 @@ def get_comp_req(game_name):
     """
     Retrieve the system requirements for a specified game.
 
-    Parameters:
-    - game_name (str): The name of the game for which to retrieve system requirements.
+    Parameters
+    ----------
+    game_name : str
+        The name of the game for which to retrieve system requirements.
 
-    Returns:
-    - pandas.DataFrame: A DataFrame containing system requirements for the specified game.
-      Columns included: 'name', 'windows', 'mac', 'linux'.
+    Returns
+    -------
+    pandas.DataFrame
+        A DataFrame containing system requirements for the specified game.
+        Columns included: 'name', 'windows', 'mac', 'linux'.
 
-    Example:
+    Example
+    -------
     >>> system_requirements = get_comp_req('Farming Simulator 17')
     >>> print(system_requirements)
-          name  windows  mac  linux
-    123  Example Game  True  False  True
+              name  windows    mac  linux
+    123  Example Game     True  False   True
     """
+
 
     requirements = all_games[all_games['name'] == game_name]
     requirements.reset_index(inplace=True)
@@ -179,17 +205,12 @@ def company_summary(company_name, developer=True):
     This function generates a summary of games associated with a specified company, including the total number of games,
     the most common sentiment among those games, and information about the price range.
 
-    Parameters:
-    - company_name (str): The name of the gaming company to summarize.
-    - developer (bool, optional): If True, the function searches for the company in the 'developers' column.
-                                  If False, it searches in the 'publishers' column. Default is True.
-
-    Returns:
-    None: The function prints a summary of the company's presence on Steam.
-
-    Example:
-    >>> company_summary('Electronic Arts', False)
+    Example
+    -------
+    >>> company_summary('Electronic Arts', developer=False)
     """
+
+    
     # Check if there are rows for both 'developers' and 'publishers'
     if not all_games[(all_games['developers'].str.contains(company_name, na=False)) &
                      (all_games['publishers'].str.contains(company_name, na=False))].empty:
